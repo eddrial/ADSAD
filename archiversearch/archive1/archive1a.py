54c9de0cfd84528dece25d61e1709c0edb83ce20
Ed Rial <eddrial@hotmail.com>
2019-10-01 16:13:37 +0200

    Initial class structure and travis.yml

---
+++ archiversearch/archive1/archive1a.py
@@ -0,0 +1,17 @@
+'''
+Created on 30 Sep 2019
+
+@author: oqb
+'''
+
+class archiver_ed():
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, name = 'My_Archiver'):
+        '''
+        Constructor
+        '''
+        self.myname = name
