8f5390f892cd4d9d46b3a2ff8d8ebd2d5fcb0e95
Ed Rial <eddrial@hotmail.com>
2019-10-02 08:08:33 +0200

    put in archiver class

---
+++ test/test_archiverdefn.py
@@ -0,0 +1,25 @@
+'''
+Created on 2 Oct 2019
+
+@author: oqb
+'''
+import unittest
+from id_analyses import archiverdefn as ad
+
+class Test(unittest.TestCase):
+
+
+    def testconstructor(self):
+        testarchive = ad.archiver_ed(self)
+        self.assertEqual(testarchive.myname, 'My_Archiver')
+        
+    def testconstructor2(self):
+        testarchive = ad.archiver_ed(self)
+        self.assertEqual(testarchive.myname, 'My_Archiver2')
+        
+    
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
