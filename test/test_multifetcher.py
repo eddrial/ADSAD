0338fb6055bde532bd93cbcb87666cea6bb5d12d
Ed Rial <eddrial@hotmail.com>
2019-10-15 14:48:52 +0200

    Start initial testing

--- test/test_multifetcher.py
+++ test/test_multifetcher.py
@@ -13,6 +13,10 @@ class Test(unittest.TestCase):
     def testName(self):
         pass
 
+    def testMultifetcherInits(self):
+        multifetcher = mf.MultiJsonFetcher()
+        self.assertIsInstance(multifetcher, mf.MultiJsonFetcher())
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
