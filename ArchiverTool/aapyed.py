c362b5a3f23b62edbc78c45beda162d10fb428e2
Ed Rial <eddrial@hotmail.com>
2019-10-15 09:49:54 +0200

    developing test script

--- ArchiverTool/aapyed.py
+++ ArchiverTool/aapyed.py
@@ -1,9 +1,33 @@
-from aa import ca
+from aa.js import JsonFetcher
 from datetime import datetime
+from tzlocal import get_localzone
+import matplotlib as dnp
 
-PRIMARY_ARCHIVER = 'http://archiver.pri.diamond.ac.uk/archive/cgi/ArchiveDataServer.cgi'
-PV = 'SR-DI-DCCT-01:SIGNAL'
+tz = get_localzone()
 
-fetcher = ca.CaFetcher(PRIMARY_ARCHIVER)
 
-start = datetime(2016, 1, 2)
+ArchiverBessy = 'archiver.bessy.de'
+CurrentBessy = 'TOPUPCC:rdCur'
+
+
+start = tz.localize(datetime(2019, 5, 2,14))
+
+end = tz.localize(datetime(2019, 5, 2, 15))
+
+jfetcher_Bessy = JsonFetcher(ArchiverBessy,80)
+
+
+#eventBessy = jfetcher_Bessy.get_values(CurrentBessy, start, end,1,{'firstFill':60})
+eventffBessy = jfetcher_Bessy.get_values(CurrentBessy, start, end, binning_params={'firstFill':60})
+eventBessy = jfetcher_Bessy.get_values(CurrentBessy, start, end)
+#plot1 = dnp.pyplot.
+
+
+event = jfetcher_Bessy.get_event_at(CurrentBessy, start)
+
+print(event.value)
+
+
+#data = fetcher._get_values(PV, start, end)
+
+print (eventBessy)
