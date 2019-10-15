988041a924a6bdcf2d062f2a58ece478f6803de7
Ed Rial <eddrial@hotmail.com>
2019-10-15 15:24:05 +0200

    Initial tests to describe class multifetcher

--- test/test_multifetcher.py
+++ test/test_multifetcher.py
@@ -16,6 +16,20 @@ class Test(unittest.TestCase):
     def testMultifetcherInits(self):
         multifetcher = mf.MultiJsonFetcher()
         self.assertIsInstance(multifetcher, mf.MultiJsonFetcher)
+        
+    def testPVsInMultiFetcherIsList(self):
+        multifetcher = mf.MultiJsonFetcher()
+        self.assertIsInstance(multifetcher.pvs, list, "PVs are not as list")
+        
+    def testTimesAreList(self):
+        multifetcher = mf.MultiJsonFetcher()
+        self.assertIsInstance(multifetcher.times, list, "times are not as list")
+        
+    def testTimesAreLength2Tuples(self):
+        multifetcher = mf.MultiJsonFetcher()
+        for time in multifetcher.times:
+            self.assertIsInstance(multifetcher.times[time], tuple, "Time " + multifetcher.times[time] + " is not a tuple")
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
