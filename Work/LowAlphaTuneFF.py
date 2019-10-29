cfa8d3c8a3a7c7a7de4bc65be23f3d7c1ca84a9d
Ed Rial <eddrial@hotmail.com>
2019-10-29 15:18:53 +0100

    Set up and grab shift archive data

--- Work/LowAlphaTuneFF.py
+++ Work/LowAlphaTuneFF.py
@@ -6,7 +6,41 @@ Created on 29 Oct 2019
 import numpy as np
 import matplotlib.pyplot as plt
 from aa import data, fetcher, js
+from tzlocal import get_localzone
+from datetime import datetime
 
 
 if __name__ == '__main__':
-    pass
+    #Set up archive
+    tz = get_localzone()
+    ArchiverBessy = 'archiver.bessy.de'
+    
+    jfetcher_Bessy = js.JsonFetcher(ArchiverBessy,80)
+    # gather data on move to create time window
+    UE112Gap = 'UE112ID7R:BasePmGap.A'
+    UE112Phase = 'UE112ID7R:SBasePmGap.B'
+    
+    phasestart = tz.localize(datetime(2019, 10, 27, 8 , 25))
+    phaseend = tz.localize(datetime(2019, 10, 27, 8, 33))
+    
+    phasemove = jfetcher_Bessy.get_values(UE112Phase, phasestart, phaseend)
+    
+    #Have a quick look
+    plt.plot(phasemove._timestamps, phasemove._values)
+    plt.show()
+    
+    # set window
+    
+    #
+    # gather more data (tune, Box0)
+    
+    # fit curves
+    
+    # create fixed x scale
+    
+    # create tables
+    
+    # compare tables
+    
+    #point to stop for debug
+    print (phasemove)
